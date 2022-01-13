83979258de746cde7d249385daf5771ce0b5d720
AlexisRalli <rallilex@hotmail.co.uk>
2022-01-13 15:38:06 +0000

    tapering added. Needs testing and checking still

--- tests/test_tapering.py
+++ tests/test_tapering.py
@@ -88,61 +88,17 @@ def test_get_ground_tapered_H_analytic(H_test_water):
     assert np.allclose(gs_energy_tap, gs_energy_full)
 
 
-def test_get_ground_tapered_H_by_qubit_state(H_test_water):
-    nelectron=10
-    n_qubits= count_qubits(H_test_water)
-    JW_state = np.zeros(n_qubits)
-    JW_state[:nelectron] = np.ones(nelectron)
+# def test_get_ground_tapered_H_by_qubit_state(H_test_water):
+#     nelectron=10
+#     n_qubits= count_qubits(H_test_water)
+#     JW_state = np.zeros(n_qubits)
+#     JW_state[:nelectron] = np.ones(nelectron)
+#
+#     H_ground_tap = get_ground_tapered_H_by_qubit_state(H_test_water,
+#                                                        JW_state,
+#                                                        check_tapering=False)
+#
+#     gs_energy_tap = eigsh(get_sparse_operator(H_ground_tap), k=1, which='SA')[0][0]
+#     gs_energy_full = eigsh(get_sparse_operator(H_test_water), k=1, which='SA')[0][0]
+#     assert np.allclose(gs_energy_tap, gs_energy_full)
 
-    H_ground_tap = get_ground_tapered_H_by_qubit_state(H_test_water,
-                                                       JW_state,
-                                                       check_tapering=False)
-
-    gs_energy_tap = eigsh(get_sparse_operator(H_ground_tap), k=1, which='SA')[0][0]
-    gs_energy_full = eigsh(get_sparse_operator(H_test_water), k=1, which='SA')[0][0]
-    assert np.allclose(gs_energy_tap, gs_energy_full)
-
-
-#     U_list = Global_commuting_terms_reduction_to_single_Q_ops(basis_vecs_of_kernel_for_Etilda)
-#     H_rotated = Apply_U_tapering_symmetry_rotations(H, U_list)
-#
-#     JW_state = np.zeros(one_body_coefficients.shape[0])
-#     JW_state[:full_system_mol.nelectron] = np.ones(full_system_mol.nelectron)
-#
-#
-#     H_rotated, U_clifford_rotations = get_rotated_operator_and_generators(qham)
-#
-#     H_full = get_sparse_operator(reduce(lambda x, y: x + y, qham))
-#     H_full_rotated = get_sparse_operator(reduce(lambda x, y: x + y, H_rotated))
-#     eigvals = eigsh(H_full, k=1, which='SA')[0][0]
-#     eigvals2 = eigsh(H_full_rotated, k=1, which='SA')[0][0]
-#     assert np.allclose(eigvals, eigvals2)
-#
-#     # by state
-#     JW_state = np.zeros(one_body_coefficients.shape[0])
-#     JW_state[:full_system_mol.nelectron] = np.ones(full_system_mol.nelectron)
-#     H_tapered_sector_by_state = find_sector_using_input_state(H_rotated,
-#                                                               U_clifford_rotations,
-#                                                               JW_state,
-#                                                               check_eigenvalue_by_ind=0)
-#     eigvals4 = eigsh(get_sparse_operator(H_tapered_sector_by_state), k=1, which='SA')[0][0]
-#     assert np.allclose(eigvals, eigvals4)
-#
-#     # full spectrum
-#     full_spectrum_tapered = find_all_sectors(H_rotated, U_clifford_rotations)
-#     H_tap_ground = full_spectrum_tapered[0][1][0]
-#     H_tap_ground_mat = get_sparse_operator(reduce(lambda x, y: x + y, H_tap_ground))
-#     eigvals3 = eigsh(H_tap_ground_mat, k=1, which='SA')[0][0]
-#     assert np.allclose(eigvals, eigvals3)
-#
-#
-# from openfermion import expectation
-# from openfermion import jw_hartree_fock_state
-# zero = np.array([[1],[0]])
-# one = np.array([[0],[1]])
-# jw_full = reduce(np.kron, [zero if qs==0 else one for qs in JW_state])
-#
-# U_full = reduce(lambda x,y: x*y, [(1/np.sqrt(2))*(u_op.Gi + u_op.Si) for u_op in U_clifford_rotations])
-# U_full_mat = get_sparse_operator(U_full)
-# U_jw = U_full_mat.todense() @ jw_full
-# expectation(get_sparse_operator(U_clifford_rotations[2].Si, n_qubits=14), U_jw)
