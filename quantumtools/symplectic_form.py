e3ec11ec69e0877af9addfec1e997e21e4a6a52e
AlexisRalli <rallilex@hotmail.co.uk>
2022-02-18 14:46:43 +0000

    symplectic kron added to build full matrix of operator

--- quantumtools/symplectic_form.py
+++ quantumtools/symplectic_form.py
@@ -1,8 +1,10 @@
-from functools import cached_property, reduce
+from functools import reduce
+from cached_property import cached_property
 from openfermion import QubitOperator
 import numpy as np
 from copy import deepcopy
 from typing import List, Union, Dict
+from scipy.sparse import csr_matrix
 
 def symplectic_to_string(symp_vec) -> str:
     """
@@ -20,7 +22,7 @@ def symplectic_to_string(symp_vec) -> str:
     X_block = symp_vec[:n_qubits]
     Z_block = symp_vec[n_qubits:]
 
-    Y_loc = ((X_block + Z_block) == 2).astype(bool)
+    Y_loc = np.bitwise_and(X_block, Z_block).astype(bool)
     X_loc = np.bitwise_xor(Y_loc, X_block).astype(bool)
     Z_loc = np.bitwise_xor(Y_loc, Z_block).astype(bool)
 
@@ -33,6 +35,61 @@ def symplectic_to_string(symp_vec) -> str:
     Pword_string = ''.join(char_aray)
 
     return Pword_string
+
+
+def count1_in_int_bitstring(i):
+    """
+    Count number of "1" bits in integar i to be thought of in binary representation
+
+    https://stackoverflow.com/questions/109023/how-to-count-the-number-of-set-bits-in-a-32-bit-integer#109025
+    https://web.archive.org/web/20151229003112/http://blogs.msdn.com/b/jeuge/archive/2005/06/08/hakmem-bit-count.aspx
+    """
+    i = i - ((i >> 1) & 0x55555555)  # add pairs of bits
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333)  # quads
+    return (((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) & 0xffffffff) >> 24
+
+
+def symplectic_to_sparse_matrix(symp_vec, coeff) -> csr_matrix:
+    """
+    Returns (2**n x 2**n) matrix of paulioperator kronector product together
+     defined from symplectic vector defined as (X | Z)
+
+    Args:
+        symp_vec (array): symplectic Pauliword array
+
+    Returns:
+        sparse_matrix (csr_matrix): sparse matrix of Pauliword
+
+    """
+    n_qubits = len(symp_vec) // 2
+
+    X_block = symp_vec[:n_qubits]
+    Z_block = symp_vec[n_qubits:]
+
+    Y_number = sum(np.bitwise_and(X_block, Z_block).astype(int))
+    global_phase = (-1j) ** Y_number
+
+    # reverse order to match bitstring int valu of each bit in binary: [..., 32, 16, 8, 4, 2, 1]
+    binary_int_array = 1 << np.arange(n_qubits-1, -1, -1)
+
+    x_int = X_block @ binary_int_array
+    z_int = Z_block @ binary_int_array
+
+    dimension = 2**n_qubits
+
+    row_ind = np.arange(dimension)
+    col_ind = np.bitwise_xor(row_ind, x_int)
+
+    row_inds_and_Zint = np.bitwise_and(row_ind, z_int)
+    vals = global_phase * (-1) ** count1_in_int_bitstring(row_inds_and_Zint)
+
+    sparse_matrix = csr_matrix(
+        (vals, (row_ind, col_ind)),
+        shape=(dimension, dimension),
+        dtype=complex
+            )
+
+    return coeff*sparse_matrix
 
 
 class PauliwordOp:
@@ -308,3 +365,17 @@ class PauliwordOp:
             OF_string = ' '.join([Pi+str(i) for i,Pi in enumerate(P_string) if Pi!='I'])
             OF_list.append(QubitOperator(OF_string, coeff_single))
         return OF_list
+
+    @cached_property
+    def to_matrix(self):
+        """
+        Function to get (2**n, 2**n) matrix of operator acting in Hilbert space
+
+        """
+        out_matrix = csr_matrix( ([],([],[])),
+                                  shape=(2**self.n_qubits,2**self.n_qubits)
+                                  )
+        for Pvec_single, coeff_single in zip(self.symp_matrix, self.coeff_vec):
+            out_matrix += symplectic_to_sparse_matrix(Pvec_single, coeff_single)
+
+        return out_matrix
